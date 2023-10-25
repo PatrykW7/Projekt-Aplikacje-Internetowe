779fc742131599b537efa90c0608fbcc579032ec
PatrykW7 <patryk90020@o2.pl>
2023-10-25 13:43:18 +0200

    Login / Register done properly

--- website/main/urls.py
+++ website/main/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("home", views.home, name="home"),
-    path("sign-up", views.sign_up, name="sing_up"),
+    path("home/", views.home, name="home"),
+    path("sign-up/", views.sign_up, name="sing_up"),
+    #path('login/', views.LoginView.as_view(template_name='core/login.html'), name = "login"),
 ]
--- website/main/views.py
+++ website/main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from .forms import RegisterForm
 from django.contrib.auth import login, logout, authenticate
 
 
