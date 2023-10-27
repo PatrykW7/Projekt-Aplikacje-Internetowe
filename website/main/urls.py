5bbec4eb5c28a19c3dab3f970ee302b6aa9cc65f
PeWeX47 <93322616+PeWeX47@users.noreply.github.com>
2023-10-27 13:02:06 +0200

    Delete website directory

--- website/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.home, name="home"),
-    path("home/", views.home, name="home"),
-    path("sign-up/", views.sign_up, name="sing_up"),
-    #path('login/', views.LoginView.as_view(template_name='core/login.html'), name = "login"),
-]
