07ac0719273e90751013a4a8f7aff0a4a12f2810
PeWeX47 <wilkos0505@gmail.com>
2023-10-28 08:54:01 +0200

    cleaned code

--- website/main/urls.py
+++ website/main/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("home/", views.home, name="home"),
     path("sign-up/", views.sign_up, name="sing_up"),
-    #path('login/', views.LoginView.as_view(template_name='core/login.html'), name = "login"),
 ]
