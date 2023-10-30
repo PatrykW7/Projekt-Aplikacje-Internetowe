c3dee229e2de150b648aa1f28b4c5e31c9b317af
PeWeX47 <wilkos0505@gmail.com>
2023-10-30 12:02:43 +0100

    added env variables

--- website/yt_sentiment/views.py
+++ website/yt_sentiment/views.py
@@ -1,60 +1,24 @@
 from django.shortcuts import render
-from transformers import pipeline, AutoTokenizer, TFAutoModelForSequenceClassification
-import googleapiclient.discovery
-import googleapiclient.errors
-import os
+from pathlib import Path
+from . import yt_comments
+from . import load_model
+from transformers import pipeline
+import environ
 
-base_path = os.path.dirname(os.path.abspath(__file__))
-model_path = os.path.join(base_path, "nlp_models", "twitter-roberta-base-sentiment")
+env = environ.Env()
+environ.Env.read_env()
 
-tokenizer = AutoTokenizer.from_pretrained(model_path)
-model = TFAutoModelForSequenceClassification.from_pretrained(model_path)
+GOOGLE_API_KEY = env("GOOGLE_API_KEY")
+model_path = Path("website\yt_sentiment\nlp_models\twitter-roberta-base-sentiment")
+model, tokenizer = load_model.load_model(path=model_path)
 
 nlp = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
-
-
-api_key = "AIzaSyDdoevpmj4XGD2YaquRlvx101kDKH94ji4"
-
-
-def get_comments(youtube, **kwargs):
-    comments = []
-    results = youtube.commentThreads().list(**kwargs).execute()
-
-    while results:
-        for item in results["items"]:
-            comment = item["snippet"]["topLevelComment"]["snippet"]["textDisplay"]
-            comments.append(comment)
-
-        # check if there are more comments
-        if "nextPageToken" in results:
-            kwargs["pageToken"] = results["nextPageToken"]
-            results = youtube.commentThreads().list(**kwargs).execute()
-        else:
-            break
-
-    return comments
-
-
-def main(video_id, api_key):
-    # Disable OAuthlib's HTTPs verification when running locally.
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-
-    youtube = googleapiclient.discovery.build("youtube", "v3", developerKey=api_key)
-
-    comments = get_comments(
-        youtube, part="snippet", videoId=video_id, textFormat="plainText"
-    )
-    return comments
-
-
-def get_video_comments(video_id):
-    return main(video_id, api_key)
 
 
 def yt_sentiment(request):
     if request.method == "POST":
         text = request.POST.get("text")
-        comments = main(text, api_key)
+        comments = yt_comments.main(text, GOOGLE_API_KEY)
         result = [nlp(comment, model)[0]["label"] for comment in comments]
         return render(request, "yt_sentiment/result.html", {"sentiment": result})
     else:
