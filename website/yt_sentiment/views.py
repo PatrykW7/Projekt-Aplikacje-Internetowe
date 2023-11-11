6c6b3c49d3559692f2e51060846b694b8c11b501
PeWeX47 <wilkos0505@gmail.com>
2023-11-11 21:41:47 +0100

    formated code

--- website/yt_sentiment/views.py
+++ website/yt_sentiment/views.py
@@ -29,18 +29,22 @@ result = {}
 @login_required(login_url="/login")
 def yt_sentiment(request):
     if request.method == "POST":
-        try :
+        try:
             text = request.POST.get("text")
             comments = get_video_comments(text, GOOGLE_API_KEY)
             if not comments:
-                result = {"available": 'No comments available'}   
-                
-                return render(request, "yt_sentiment/yt_sentiment.html", {'sentiment':result})
+                result = {"available": "No comments available"}
+
+                return render(
+                    request, "yt_sentiment/yt_sentiment.html", {"sentiment": result}
+                )
             else:
                 analysed_comments = [nlp(comment, model)[0] for comment in comments]
                 df_result = pd.DataFrame(analysed_comments)
                 # ZASTANOWIC SIE CO LEPSZE sentiment_percentage, czy percents
-                sentiment_percentage = df_result["label"].value_counts().values / len(df_result)
+                sentiment_percentage = df_result["label"].value_counts().values / len(
+                    df_result
+                )
                 sentiment_percentage = [round(num, 2) for num in sentiment_percentage]
                 num_comments = len(df_result)
                 percents = df_result["label"].value_counts()
@@ -52,11 +56,12 @@ def yt_sentiment(request):
                 labels = list(percent_help.keys())
                 values = list(percent_help.values())
 
-
         except HttpError:
-            result = {"available": 'No comments available'} 
-                   
-            return render(request, "yt_sentiment/yt_sentiment.html", {'sentiment':result})
+            result = {"available": "No comments available"}
+
+            return render(
+                request, "yt_sentiment/yt_sentiment.html", {"sentiment": result}
+            )
 
         try:
             most_pos_com_id = df_result[df_result["label"] == "positive"][
@@ -81,10 +86,8 @@ def yt_sentiment(request):
             "most_pos_com": most_pos_com,
             "most_neg_com": most_neg_com,
             "percents": percent_help.items,
-            "labels" : labels,
-            "values" : values
-         
-
+            "labels": labels,
+            "values": values,
         }
 
         #### I WYKRES
@@ -93,15 +96,15 @@ def yt_sentiment(request):
         sizes = [45.45, 45.45, 9.09]
 
         # Utwórz obiekt wykresu kołowego
-        fig = px.pie(labels=labels, values=sizes, title='Wykres kołowy')
+        fig = px.pie(labels=labels, values=sizes, title="Wykres kołowy")
 
         # Konwertuj wykres do HTML
         plot_html = fig.to_html(full_html=False)
 
-
-        
-
-
-        return render(request, "yt_sentiment/yt_sentiment.html", {"sentiment": result, 'plot_html': plot_html})
+        return render(
+            request,
+            "yt_sentiment/yt_sentiment.html",
+            {"sentiment": result, "plot_html": plot_html},
+        )
     else:
         return render(request, "yt_sentiment/yt_sentiment.html")
