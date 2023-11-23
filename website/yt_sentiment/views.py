e802038ab002a331be48f318a97af0b34b1165a2
PatrykW7 <patryk90020@o2.pl>
2023-11-23 21:23:02 +0100

    Added Table with comments

--- website/yt_sentiment/views.py
+++ website/yt_sentiment/views.py
@@ -8,6 +8,7 @@ import pandas as pd
 from django.contrib.auth.decorators import login_required
 from urllib.error import HTTPError
 from googleapiclient.errors import HttpError
+import pandas as pd
 
 env = environ.Env()
 environ.Env.read_env()
@@ -31,6 +32,11 @@ def yt_sentiment(request):
         try:
             text = request.POST.get("text")
             comments = get_video_comments(text, GOOGLE_API_KEY)
+
+            
+           
+
+
             if not comments:
                 result = {"available": "No comments available"}
 
@@ -47,6 +53,7 @@ def yt_sentiment(request):
                 sentiment_percentage = [round(num, 2) for num in sentiment_percentage]
                 num_comments = len(df_result)
                 percents = df_result["label"].value_counts()
+                lab = df_result["label"]
                 percent_help = {}
 
                 for label, value in zip(percents.index, percents.values):
@@ -54,6 +61,14 @@ def yt_sentiment(request):
 
                 labels = list(percent_help.keys())
                 values = list(percent_help.values())
+
+                index = []
+                [index.append(i+1) for i in range(len(comments))]
+                df_comments = pd.DataFrame({
+                    'comments': comments,
+                    'labels': lab, 
+                })
+
 
         except HttpError:
             result = {"available": "No comments available"}
@@ -86,12 +101,13 @@ def yt_sentiment(request):
             "percents": percent_help.items,
             "labels": labels,
             "values": values,
+            
         }
 
         return render(
             request,
             "yt_sentiment/yt_sentiment.html",
-            {"sentiment": result},
+            {"sentiment": result, "df": df_comments},
         )
     else:
         return render(request, "yt_sentiment/yt_sentiment.html")
